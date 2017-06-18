2536d444e36392abf25098ea6ca09c489a3c556f
lihui13603981029 <397727593@qq.com>
2017-06-18 20:41:06 +0800

    add 哈哈

--- nodejsTest.js
+++ nodejsTest.js
@@ -3,6 +3,7 @@ http.createServer(function(request, response) {
     response.writeHead(200, {
         "Content-Type" : "text/plain"
     });
+    console.log("这是一个修改");
     response.write("Welcome to Nodejs");
     response.end();
 }).listen(8000, "127.0.0.1");
@@ -10,3 +11,4 @@ console.log("");
 console.log("Creat server on http://127.0.0.1:8000/");
 console.log("这是一个不错的教程,我学会的很多东西,测试1234");
 console.log("kakakak");
+
