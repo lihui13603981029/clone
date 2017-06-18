a4a7cb4e120878cc9787d0f70916d676bd578d8d
lihui13603981029 <397727593@qq.com>
2017-06-18 14:23:14 +0800

    commit

--- nodejsTest.js
+++ nodejsTest.js
@@ -6,7 +6,6 @@ http.createServer(function(request, response) {
     response.write("Welcome to Nodejs");
     response.end();
 }).listen(8000, "127.0.0.1");
-console.log("这是第二笔提交");
+console.log("这是第二笔提交号码");
 console.log("Creat server on http://127.0.0.1:8000/");
-console.log("这是一个不错的教程,我学会的很多东西,测试1234");
-console.log();
+console.log("这是一个不错的教程,我学会的很多东西,测试1234");console.log();
